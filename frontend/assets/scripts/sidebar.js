2927d1e2be022a495a3891c654089a751afe2bc4
wnicolass <nicolaswgl99@gmail.com>
2023-02-23 21:35:59 +0000

    feat: end navigation and sidebar

--- frontend/assets/scripts/sidebar.js
+++ frontend/assets/scripts/sidebar.js
@@ -1,6 +1,7 @@
 const hamburguerBtn = document.getElementById('mobile-menu-btn');
 const closeSideBarBtn = document.getElementById('close-btn');
 const sideBar = document.getElementById('mobile-menu');
+const sideBarBackground = sideBar.previousElementSibling;
 
 function showSideBar() {
   sideBar.classList.add('active');
@@ -10,5 +11,8 @@ function closeSideBar() {
   sideBar.classList.remove('active');
 }
 
-closeSideBarBtn.addEventListener('click', closeSideBar);
+[closeSideBarBtn, sideBarBackground].forEach((closable) => {
+  closable.addEventListener('click', closeSideBar);
+});
+
 hamburguerBtn.addEventListener('click', showSideBar);
